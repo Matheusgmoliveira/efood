8e964db69afb8577ca2b94a944ec389d42e8eb0a
Matheus Gonçalves <matheusgoncalves1970@gmail.com>
2024-12-07 15:42:08 -0300

    corrigindo tamanho footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,17 +3,15 @@ import Logo from '../../assets/images/logo.png'
 import RedesSociais from '../../assets/images/redes sociais.png'
 const Footer = () => (
   <div>
-    <div className="container">
-      <Container>
-        <img src={Logo}></img>
-        <img src={RedesSociais} style={{ marginTop: '32px' }}></img>
-        <Descricao>
-          A efood é uma plataforma para divulgação de estabelecimentos, a
-          responsabilidade pela entrega, qualidade dos produtos é toda do
-          estabelecimento contratado.{' '}
-        </Descricao>
-      </Container>
-    </div>
+    <Container>
+      <img src={Logo}></img>
+      <img src={RedesSociais} style={{ marginTop: '32px' }}></img>
+      <Descricao>
+        A efood é uma plataforma para divulgação de estabelecimentos, a
+        responsabilidade pela entrega, qualidade dos produtos é toda do
+        estabelecimento contratado.{' '}
+      </Descricao>
+    </Container>
   </div>
 )
 
