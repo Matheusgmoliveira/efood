f046d9670b391a35492df6938fd56e856255ab7a
Matheus Gonçalves <matheusgoncalves1970@gmail.com>
2025-10-24 06:29:34 -0300

    Atualiza banner

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -14,75 +14,73 @@ export const Container = styled.section`
 export const Modal = styled.div`
   display: none;
   position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  inset: 0;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
 
   &.visivel {
     display: flex;
   }
-  .container {
-    max-width: 1024px;
-  }
 
   .overlay {
     position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+    inset: 0;
     background-color: rgba(0, 0, 0, 0.73);
+    z-index: 1;
   }
 `
+
 export const Banner = styled.div`
   display: flex;
-  justify-content: space-between;
+  gap: 32px;
   width: 1024px;
   height: 344px;
-  position: relative;
-  margin: 490px 171px 790px 171px;
   background-color: ${cores.salmao};
-  z-index: 1;
+  padding: 32px;
+
+  position: relative;
+  z-index: 2;
 `
 
 export const ImagemProduto = styled.img`
-  margin: 32px;
+  width: 280px;
+  height: auto;
 `
 
 export const Nome = styled.p`
-  margin-top: 32px;
   font-weight: 900;
   font-size: 18px;
-  line-height: 100%;
   color: ${cores.branco};
+  margin-bottom: 16px;
 `
 export const Detalhes = styled.p`
-  width: 656px;
-  margin-top: 16px;
-  font-weight: 400;
+  max-width: 600px;
   font-size: 14px;
   line-height: 22px;
   color: ${cores.branco};
+  margin-bottom: 16px;
 `
-export const Informacao = styled(Detalhes)``
+
+export const Informacao = styled(Detalhes)`
+  margin-bottom: 24px;
+`
 
 export const BotaoCarrinho = styled.button`
-  width: 218px;
   height: 24px;
-  font-size: 14px;
-  font-weight: 700;
-  text-align: center;
   background-color: ${cores.rosaClaro};
   color: ${cores.salmao};
-  margin-top: 16px;
+  font-weight: 700;
+  font-size: 14px;
+  padding: 4px 16px;
   border: none;
   cursor: pointer;
 
   &:hover {
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    opacity: 0.9;
   }
 `
+
 export const Close = styled.img`
   position: absolute;
   top: 16px;
